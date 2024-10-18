113583b791472cb3abd8c72ab042372ae646c296
Alex Thagichu <lexthagz.atk@gmail.com>
2024-10-18 09:16:25 +0000

    centering div

--- src/sections/MeetTheCharacters.jsx
+++ src/sections/MeetTheCharacters.jsx
@@ -91,7 +91,7 @@ const MeetTheCharacters = () => {
 
   return (
     <div className="meet-the-characters pt-24 pb-16 sm:pb-24 sm:pt-32 xl:pb-32 bg-gray-20">
-      <div className="max-w-[810px] mx-auto text-center mb-[52px]">
+      <div className="mx-auto text-center mb-[52px]">
       <h2 className="meet-the-characters text-5xl text-gray-800 font-extrabold mx-auto md:text-5xl mb-12">
         Meet The Characters
       </h2>
